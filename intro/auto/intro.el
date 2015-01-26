3f68468a6abb2f42e63771daa21f515cd1fb77aa
Michael D Jones <biotechjones@gmail.com>
2015-01-26 15:04:08 -0800

    Made new folders for chapters, general clean

---
+++ intro/auto/intro.el
@@ -0,0 +1,11 @@
+(TeX-add-style-hook
+ "intro"
+ (lambda ()
+   (LaTeX-add-labels
+    "ch:Introduction"
+    "sec:s_layer_structure"
+    "fig:cellwalls"
+    "fig:symmetries"
+    "sec:history_of_s_layers"
+    "fig:firstslayer")))
+
